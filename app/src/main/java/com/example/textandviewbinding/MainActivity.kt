d913c5799bed3911eeaf2b1db2a146ae079221ad
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-02-24 20:56:20 +0000

    Configured viewBinding in MainActivity.kt

--- app/src/main/java/com/example/textandviewbinding/MainActivity.kt
+++ app/src/main/java/com/example/textandviewbinding/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.textandviewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.textandviewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Initializing bindings
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Inflate the activity binding
+        binding = ActivityMainBinding.inflate(layoutInflater )
+        val view = binding.root
+        setContentView(view)
+        // setContentView(R.layout.activity_main)
+
     }
 }
