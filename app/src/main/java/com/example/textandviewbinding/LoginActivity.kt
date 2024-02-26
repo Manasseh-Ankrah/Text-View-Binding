78b56f2aebc10839b72f91238a5e2dc6b880600b
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-02-26 01:12:31 +0000

    Added Snackbar with button

--- app/src/main/java/com/example/textandviewbinding/LoginActivity.kt
+++ app/src/main/java/com/example/textandviewbinding/LoginActivity.kt
@@ -2,8 +2,11 @@ package com.example.textandviewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.textandviewbinding.databinding.LoginMainBinding
+import com.google.android.material.snackbar.Snackbar
+import java.util.Calendar
 
 class LoginActivity : AppCompatActivity() {
     //Initializing bindings
@@ -25,11 +28,22 @@ class LoginActivity : AppCompatActivity() {
             // Local Authentication process
             if (username.toString().equals("mandev",true) && password.toString().equals("123")) {
                 //Display Toast when user credential matches the stated values
-                Toast.makeText(this , "Welcome to Android Kotlin", Toast.LENGTH_SHORT).show()
+                //  Toast.makeText(this , "Welcome to Android Kotlin", Toast.LENGTH_SHORT).show()
+                // Display Normal Snackbar
+                //Snackbar.make(it,"Hi, $username welcome to Android Kotlin",Snackbar.LENGTH_LONG).show()
+                //Display snackbar with button
+                val snack = Snackbar.make(it,"Hi, $username welcome to Android Kotlin",Snackbar.LENGTH_LONG)
+                snack.setAction("Details", {displayToast()})
+                snack.show()
             } else {
-                Toast.makeText(this , "Invalid Login Attempt", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this , "Invalid Login Attempt", Toast.LENGTH_SHORT).show()
+                Snackbar.make(it,"Login Succesful",Snackbar.LENGTH_LONG).show()
             }
         }
 
+    }
+
+    private fun displayToast() {
+        Toast.makeText(this , "Invalid Login Attempt ${Calendar.getInstance().time}", Toast.LENGTH_SHORT).show()
     }
 }
